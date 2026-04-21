92008ac97e2154851a2d528b0c2915415c34124d
Vitorinka <mishurvika@gmail.com>
2026-04-21 20:47:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
+@dp.message_handler(content_types=['photo'])
+async def get_file_id(message: types.Message):
+    # Берем ID самого качественного (последнего) фото
+    file_id = message.photo[-1].file_id
+    await message.answer(f"Твой file_id:\n<code>{file_id}</code>", parse_mode='HTML')import os
 import stripe
 import asyncio
 import logging
