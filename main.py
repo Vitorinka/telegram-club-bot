24382fd4482b9fbca54f1f552aca2200f9146fb9
Vitorinka <mishurvika@gmail.com>
2026-05-24 15:15:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -316,6 +316,11 @@ async def promo_cancel(callback: types.CallbackQuery, state: FSMContext):
     await state.finish()
     await callback.answer()
 
+@dp.message_handler(content_types=['video'], state=None)
+async def reply_with_video_id(message: types.Message):
+    file_id = message.video.file_id
+    await message.reply(f"Ваш video file_id:\n`{file_id}`", parse_mode="Markdown")
+
 # --- ХЕНДЛЕРЫ КОМАНД И КОЛБЭКОВ ---
 @dp.message_handler(commands=['start'], state='*')
 async def start(message: types.Message, state: FSMContext):
